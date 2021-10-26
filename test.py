938a5976f1f3326b952e78155251afb552fe86c1
AnkitMrt <92021698+AnkitMrt@users.noreply.github.com>
2021-10-26 11:34:31 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 import unittest
-import script
+import script1
 
 class Testing(unittest.TestCase):
   def test_divBy3(self):
-    self.assertAlmostEqual(script.div3(222),True)
-    self.assertAlmostEqual(script.div3(19),False)
+    self.assertAlmostEqual(script1.div3(222),True)
+    self.assertAlmostEqual(script1.div3(19),False)
  
 if __name__=='__main__':
   unittest.main()
