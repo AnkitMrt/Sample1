c706205c863333cc368a400c43f05267c1c138a6
AnkitMrt <92021698+AnkitMrt@users.noreply.github.com>
2021-10-26 11:39:29 +0530

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import script1
 
 class Testing(unittest.TestCase):
   def test_divBy3(self):
-#     self.assertAlmostEqual(script1.div3(222),True)
+    self.assertAlmostEqual(script1.div3(222),True)
     self.assertAlmostEqual(script1.div3(19),False)
  
 if __name__=='__main__':
